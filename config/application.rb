f5971841091447a0fd847cc4306dc9ca6c499ef1
mbie <embielec@gmail.com>
2017-04-21 23:09:46 +0200

    Add app name

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module Mongrel
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.app_name = 'mongrel'
   end
 end
